4a163c42381e49b01f7b46df1e650ef1901c55ac
thuyptt610 <thanhthuy0983860756@gmail.com>
2024-08-08 21:58:55 +0700

    add chuong3 slide24-cau3: Xoa khoang trang thua trong chuoi

--- Chuong3/Chuong3_slide24.cpp
+++ Chuong3/Chuong3_slide24.cpp
@@ -27,7 +27,26 @@ int demKhoangTrang(const char* s)
 	}
 	return count;
 }
+//3. xoa khoang trang thua trong chuoi
+void xoa_khoang_trang_thua(char* s) {
+	char* dst = s;
+	int space_found = 0;
 
+	while (*s) {
+		if (*s == ' ') {
+			if (!space_found) {
+				*dst++ = *s;
+				space_found = 1;
+			}
+		}
+		else {
+			*dst++ = *s;
+			space_found = 0;
+		}
+		s++;
+	}
+	*dst = '\0';
+}
 
 int main() {
 	int lua_chon;
@@ -66,7 +85,16 @@ int main() {
 			printf("So khoang trang trong chuoi la: %d", demKhoangTrang(s));
 			break;
 		}
-
+		case 3: {
+			char s[100];
+			printf("Nhap chuoi can xoa khoang trang thua: ");
+			while (getchar() != '\n');
+			fgets(s, sizeof(s), stdin);
+			s[strcspn(s, "\n")] = '\0';
+			xoa_khoang_trang_thua(s);
+			printf("Chuoi sau khi xoa khoang trang thua: %s\n", s);
+			break;
+		}
 	
 		default:
 			printf("Lua chon khong hop le!\n");
