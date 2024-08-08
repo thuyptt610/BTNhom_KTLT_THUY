cdb30cd33ccea84a961ee8d792105389ad975b0b
thuyptt610 <thanhthuy0983860756@gmail.com>
2024-08-08 11:31:45 +0700

    add chuong2 slide15-cau4: tim kiem x trong a theo tim kiem tuyen tinh/ tim kiem nhi phan

--- Chuong2/Slide15-16.cpp
+++ Chuong2/Slide15-16.cpp
@@ -43,12 +43,40 @@ void demChanLe(int a[], int n) {
     printf("\nSo phan tu chan la: %d\n", chan);
     printf("So phan tu le la: %d\n", le);
 }
+// tim kiem tuyen tinh
+int timKiemTuyenTinh(int a[], int n, int x) {
+    for (int i = 0; i < n; i++) {
+        if (a[i] == x) {
+            return i; // Tra ve vi tri dau tien tim thay
+        }
+    }
+    return -1; // Khong tim thay
+}
+// Tim kiem nhi phan
+int timKiemNhiPhan(int a[], int n, int x) {
+    int left = 0;
+    int right = n - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (a[mid] == x) {
+            return mid; // Tra ve vi tri tim thay
+        }
+        else if (a[mid] < x) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+    return -1; // Khong tim thay
+}
 void menu() {
     printf("\n--- Menu ---\n");
     printf("1. Nhap mang/ xuat mang a\n");
     printf("2. Tim gia tri max/min\n");
     printf("3. Dem so luong chan/le\n");
- 
+    printf("4(4.1). Tim kiem tuyen tinh\n");
+    printf("5(4.2). Tim kiem nhi phan\n");
    
 }
 int main() {
@@ -56,7 +84,8 @@ int main() {
     int n, choice, x;
     int a[50];
     srand(time(0)); 
-
+    int viTriTuyenTinh;
+    int viTriNhiPhan;
     do {
 
         printf("Nhap lua chon cua ban: ");
@@ -80,6 +109,28 @@ int main() {
             break;
         case 3:
             demChanLe(a, n);
+            break;
+        case 4:
+            printf("Nhap gia tri x de tim trong mang: ");
+            scanf_s("%d", &x);
+            viTriTuyenTinh = timKiemTuyenTinh(a, n, x);
+            if (viTriTuyenTinh != -1) {
+                printf("Gia tri %d xuat hien tai vi tri %d trong mang.\n", x, viTriTuyenTinh);
+            }
+            else {
+                printf("Gia tri %d khong co trong mang.\n", x);
+            }
+            break;
+        case 5:
+            printf("Nhap gia tri x de tim trong mang: ");
+            scanf_s("%d", &x);
+            viTriNhiPhan = timKiemNhiPhan(a, n, x);
+            if (viTriNhiPhan != -1) {
+                printf("Gia tri %d xuat hien tai vi tri %d trong mang.\n", x, viTriNhiPhan);
+            }
+            else {
+                printf("Gia tri %d khong co trong mang.\n", x);
+            }
             break;
         case 12:
             printf("Thoat chuong trinh.\n");
