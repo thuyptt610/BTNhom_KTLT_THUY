ed86dd9188b4b786cb7b88c687062b7233169d92
thuyptt610 <thanhthuy0983860756@gmail.com>
2024-08-10 17:43:51 +0700

    add chuong4 slide29-bai11: Viet ham xuat day co n chu so Fibonacci...

---
+++ Chuong4/chuong4-slide29-Bai11.cpp
@@ -0,0 +1,39 @@
+﻿#include <stdio.h>
+
+// Xuat day Fibonacci
+
+int fibonacciRecursive(int n);
+void printFibonacci(int n);
+
+int main() {
+    int n;
+    do {
+
+        printf("Nhap so luong so Fibonacci can xuat: ");
+        scanf_s("%d", &n);
+
+        if (n <= 0) {
+            printf("So luong phan tu phai la so nguyen duong!\n");
+        }
+    } while (n <= 0);
+
+    printFibonacci(n);
+
+    return 0;
+}
+
+// Hàm đệ quy tính số Fibonacci thứ n
+int fibonacciRecursive(int n) {
+    if (n <= 2) {
+        return 1; 
+    }
+    return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+}
+
+// Hàm xuất dãy Fibonacci 
+void printFibonacci(int n) {
+    for (int i = 1; i <= n; i++) {
+        printf("%d ", fibonacciRecursive(i));
+    }
+    printf("\n");
+}
