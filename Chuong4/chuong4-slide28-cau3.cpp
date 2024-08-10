b8a373f5e2d155b2b5067abbcf7565e4dc2702b2
thuyptt610 <thanhthuy0983860756@gmail.com>
2024-08-10 16:55:48 +0700

    add Chuong4 slide28-cau3: Viet ham tim so nguyen lon nhat cua 2 so a va b

---
+++ Chuong4/chuong4-slide28-cau3.cpp
@@ -0,0 +1,45 @@
+﻿#include <stdio.h>
+
+// Hàm đệ quy  tính UCLN của a và b theo phương pháp Euclid 
+int gcdRecursive(int a, int b);
+
+int main() {
+    int a, b;
+
+    do {
+        printf("Nhap so nguyen duong a: ");
+        scanf_s("%d", &a);
+
+        if (a <= 0) {
+            printf("a phai la so nguyen duong!\n");
+        }
+    } while (a <= 0);
+
+    do {
+        printf("Nhap so nguyen duong b: ");
+        scanf_s("%d", &b);
+
+        if (b <= 0) {
+            printf("b phai la so nguyen duong!\n");
+        }
+    } while (b <= 0);
+
+    int result = gcdRecursive(a, b);
+
+    printf("UCLN cua %d va %d la %d\n", a, b, result);
+
+    return 0;
+}
+
+int gcdRecursive(int a, int b) {
+    if (b == 0) {
+        return a;
+    }
+    else if (a > b) {
+        return gcdRecursive(b, a - b);
+    }
+    else {
+        return gcdRecursive(a, b - a);
+    }
+}
+
