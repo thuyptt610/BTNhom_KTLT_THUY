2a45790430df62072610c15b266eb10ad3a8adf0
pthanhthuy95 <pthithanhthuy59@gmail.com>
2024-08-10 12:08:23 +0700

    add chuong4-slide13-bai3: Hàm đệ quy tính giá trị phần tử thứ n của cấp số nhân  Un={(a,nếu n=1 và q*Un-1,nếu n >1)

---
+++ Chuong4/Chuong4_Slide13-Bai3.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+// Hàm đệ quy tính giá trị phần tử thứ n của cấp số nhân  Un={(a,nếu n=1 và q*Un-1,nếu n >1)
+double U_n(int n, double a, double q) {
+    if (n == 1) {
+        return a;
+    }
+    else {
+        return q * U_n(n - 1, a, q);
+    }
+}
+int main() {
+    int n;
+    double a, q;
+    printf("Nhap gia tri cua n: ");
+    scanf_s("%d", &n);
+    printf("Nhap gia tri cua a: ");
+    scanf_s("%lf", &a);
+    printf("Nhap gia tri cua q: ");
+    scanf_s("%lf", &q);
+    printf("Gia tri cua phan tu thu %d cua cap so nhan la: %.2lf\n", n, U_n(n, a, q));
+
+    return 0;
+}
